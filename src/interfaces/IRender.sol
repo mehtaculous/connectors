e136ca724f27b95f3f24a04b353a6b6c2c15e647
Sahil Mehta <samehta91@gmail.com>
2023-01-13 21:58:47 -0800

    Fix bug

--- src/interfaces/IRender.sol
+++ src/interfaces/IRender.sol
@@ -16,7 +16,7 @@ interface IRender {
 
     function YELLOW() external view returns (string memory);
 
-    function renders(
+    function displays(
         uint256
     ) external view returns (string memory base, string memory player1, string memory player2);
 
